1758fd2d0b2bfb9b114973520d19f119a7793298
FREAK-HEAD07 <86604386+FREAK-HEAD07@users.noreply.github.com>
2021-07-11 11:11:54 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -3,9 +3,9 @@ del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
 net config server /srvcomment:"Windows Server 2019 By faris" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
-net user farisadmin accessrdp /add >nul
-net localgroup administrators farisadmin /add >nul
-net user farisadmin /active:yes >nul
+net user farisadministrator accessrdp /add >nul
+net localgroup administrators farisadministrator /add >nul
+net user farisadministrator /active:yes >nul
 net user installer /delete
 diskperf -Y >nul
 sc config Audiosrv start= auto >nul
@@ -15,7 +15,7 @@ ICACLS C:\Windows\installer /grant farisadmin:F >nul
 echo Successfully Installed !, If the RDP is Dead, Please Rebuild Again!
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Tidak bisa mendapatkan NGROK tunnel, pastikan NGROK_AUTH_TOKEN benar di Settings> Secrets> Repository secret. Mungkin VM Anda sebelumnya masih berjalan: https://dashboard.ngrok.com/status/tunnels "
-echo Username: farisadmin
+echo Username: farisadministrator
 echo Password: @Accessrdp
 echo Silahkan Login Ke RDP Anda!!
 ping -n 10 127.0.0.1 >nul
