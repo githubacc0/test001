2bbb09fe322ecb13490ac6865947caba1654d6e1
FREAK-HEAD07 <86604386+FREAK-HEAD07@users.noreply.github.com>
2021-07-25 21:31:58 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -12,7 +12,7 @@ sc config Audiosrv start= auto >nul
 sc start audiosrv >nul
 ICACLS C:\Windows\Temp /grant FarisRDP:F >nul
 ICACLS C:\Windows\installer /grant FarisRDP:F >nul
-echo Successfully Installed !, If the RDP is Dead, Please Rebuild Again!
+echo Successfully Installed !, If the RDP is Dead
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Tidak bisa mendapatkan NGROK tunnel, pastikan NGROK_AUTH_TOKEN benar di Settings> Secrets> Repository secret. Mungkin VM Anda sebelumnya masih berjalan: https://dashboard.ngrok.com/status/tunnels "
 echo Username: FarisRDP
