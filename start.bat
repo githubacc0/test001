29e2777256c6ebf34819f89495f4265b2aa5153d
FREAK-HEAD07 <86604386+FREAK-HEAD07@users.noreply.github.com>
2021-07-25 21:34:39 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -4,14 +4,14 @@ net config server /srvcomment:"Windows Server 2019 By Farisrdp" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
 net user FarisRDP @AccessFarisrdp /add >nul
-net localgroup administrators FarisRDP /add >nul
-net user FarisRDP /active:yes >nul
+net localgroup administrators administrator /add >nul
+net user administrator /active:yes >nul
 net user installer /delete
 diskperf -Y >nul
 sc config Audiosrv start= auto >nul
 sc start audiosrv >nul
-ICACLS C:\Windows\Temp /grant FarisRDP:F >nul
-ICACLS C:\Windows\installer /grant FarisRDP:F >nul
+ICACLS C:\Windows\Temp /grant administrator:F >nul
+ICACLS C:\Windows\installer /grant administrator:F >nul
 echo Successfully Installed !, If the RDP is Dead
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Tidak bisa mendapatkan NGROK tunnel, pastikan NGROK_AUTH_TOKEN benar di Settings> Secrets> Repository secret. Mungkin VM Anda sebelumnya masih berjalan: https://dashboard.ngrok.com/status/tunnels "
